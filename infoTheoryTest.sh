cf6e46a9c77c1cfc2599773bbd1471e3ca364c5c
Joel <joel.wallenberg@gmail.com>
2018-07-18 17:49:51 +0100

    updated shell script

--- infoTheoryTest.sh
+++ infoTheoryTest.sh
@@ -1,12 +1,12 @@
 CS="java -classpath ~/CurrentLx/OldNorse/CS_2.003.04.jar csearch/CorpusSearch"
 
-pceec="/Users/yoelw/CurrentLx/OldNorse/hnps/pceec/psd/*.psd"
+pceec="~/CurrentLx/OldNorse/hnps/pceec/psd/*.psd"
 
-midfre="/Users/yoelw/CurrentLx/OldNorse/MiddleFrench/*.psd"
+midfre="~/CurrentLx/OldNorse/MiddleFrench/*.psd"
 
-icepahc="/Users/yoelw/icecorpus/finished/*.psd"
+icepahc="~/icecorpus/finished/*.psd"
 
-ycoe="/Users/yoelw/CurrentLx/OldNorse/historicalsyntaxcourse/YCOE/*.psd"
+ycoe="~/CurrentLx/OldNorse/historicalsyntaxcourse/YCOE/*.psd"
 
 ppcmbe="~/CurrentLx/OldNorse/ppche4/PPCMBE2-RELEASE-1/corpus/psd/*.psd"
 ppcme2="~/CurrentLx/OldNorse/ppche4/PPCME2-RELEASE-4/corpus/psd/*.psd"
@@ -17,10 +17,36 @@ tychobrahe="/Users/yoelw/CurrentLx/OldNorse/tychobrahe/*psd.txt"
 #Run Study on PPCHE
 
 
-rm myovvo.ymeb.cod
-$CS myovvo.ymeb.c $ycoe $ppcme2 $ppceme $ppcmbe -out myovvo.ymeb.cod
+rm infoTheoryTest.ymeb.cod
+$CS infoTheoryTest.c $ycoe $ppcme2 $ppceme $ppcmbe -out infoTheoryTest.ymeb.cod
 
-rm myovvo.ymeb.cod.ooo
-$CS codes.q myovvo.ymeb.cod
+rm infoTheoryTest.ymeb.cod.ooo
+$CS codes.q infoTheoryTest.ymeb.cod
+
+"Done with study on ppche"
+
+python fixIdFormat.py < infoTheoryTest.ymeb.cod.ooo > infoTheoryTest.ymeb.cod.ooo.tmp
+mv infoTheoryTest.ymeb.cod.ooo.tmp infoTheoryTest.ymeb.cod.ooo
+
+echo "Fixed codes"
+
+rm infoTheoryTest.ice.nodates.cod
+$CS infoTheoryTest.c $icepahc -out infoTheoryTest.ice.nodates.cod
+
+echo "done with study on IcePaHC"
+
+$CS dateGenre.ice.c infoTheoryTest.ice.nodates.cod -out infoTheoryTest.ice.cod
+
+echo "done with dating IcePaHC data"
+
+rm infoTheoryTest.ice.cod.ooo
+$CS codes.q infoTheoryTest.ice.cod
+
+"Done with study on icepahc"
+
+python fixIdFormat.py < infoTheoryTest.ice.cod.ooo > infoTheoryTest.ice.cod.ooo.tmp
+mv infoTheoryTest.ice.cod.ooo.tmp infoTheoryTest.ice.cod.ooo
+
+echo "Fixed codes"
 
 Rscript myovvo_plotsonly.R
