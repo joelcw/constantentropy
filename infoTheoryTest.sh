c99c851ce59a900a39a33642c787858f725ab097
Joel <joel.wallenberg@gmail.com>
2018-09-11 14:45:05 +0100

    fixed directory name

--- infoTheoryTest.sh
+++ infoTheoryTest.sh
@@ -21,31 +21,31 @@ rm infoTheoryTest.ymeb.cod
 $CS infoTheoryTest.c $ycoe $ppcme2 $ppceme $ppcmbe -out infoTheoryTest.ymeb.cod
 
 rm infoTheoryTest.ymeb.cod.ooo
-$CS codes.q queriesandoutput/infoTheoryTest.ymeb.cod
+$CS codes.q outputs/infoTheoryTest.ymeb.cod
 
 "Done with study on ppche"
 
-python fixIdFormat.py < queriesandoutput/infoTheoryTest.ymeb.cod.ooo > queriesandoutput/infoTheoryTest.ymeb.cod.ooo.tmp
-mv queriesandoutput/infoTheoryTest.ymeb.cod.ooo.tmp queriesandoutput/infoTheoryTest.ymeb.cod.ooo
+python fixIdFormat.py < outputs/infoTheoryTest.ymeb.cod.ooo > outputs/infoTheoryTest.ymeb.cod.ooo.tmp
+mv outputs/infoTheoryTest.ymeb.cod.ooo.tmp outputs/infoTheoryTest.ymeb.cod.ooo
 
 echo "Fixed codes"
 
-rm queriesandoutput/infoTheoryTest.ice.nodates.cod
-$CS infoTheoryTest.c $icepahc -out queriesandoutput/infoTheoryTest.ice.nodates.cod
+rm outputs/infoTheoryTest.ice.nodates.cod
+$CS infoTheoryTest.c $icepahc -out outputs/infoTheoryTest.ice.nodates.cod
 
 echo "done with study on IcePaHC"
 
-$CS dateGenre.ice.c queriesandoutput/infoTheoryTest.ice.nodates.cod -out queriesandoutput/infoTheoryTest.ice.cod
+$CS dateGenre.ice.c outputs/infoTheoryTest.ice.nodates.cod -out outputs/infoTheoryTest.ice.cod
 
 echo "done with dating IcePaHC data"
 
-rm queriesandoutput/infoTheoryTest.ice.cod.ooo
-$CS codes.q queriesandoutput/infoTheoryTest.ice.cod
+rm outputs/infoTheoryTest.ice.cod.ooo
+$CS codes.q outputs/infoTheoryTest.ice.cod
 
 "Done with study on icepahc"
 
-python fixIdFormat.py < queriesandoutput/infoTheoryTest.ice.cod.ooo > queriesandoutput/infoTheoryTest.ice.cod.ooo.tmp
-mv queriesandoutput/infoTheoryTest.ice.cod.ooo.tmp queriesandoutput/infoTheoryTest.ice.cod.ooo
+python fixIdFormat.py < outputs/infoTheoryTest.ice.cod.ooo > outputs/infoTheoryTest.ice.cod.ooo.tmp
+mv outputs/infoTheoryTest.ice.cod.ooo.tmp outputs/infoTheoryTest.ice.cod.ooo
 
 echo "Fixed codes"
 
