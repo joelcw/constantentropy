fe22d99b3c9e492459a7c975616e361d106b40ae
Joel <joel.wallenberg@gmail.com>
2018-09-19 14:45:15 +0100

    fixed various scripts

--- infoTheoryTest.sh
+++ infoTheoryTest.sh
@@ -1,39 +1,44 @@
-CS="java -classpath ~/CurrentLx/OldNorse/CS_2.003.04.jar csearch/CorpusSearch"
+CS="java -classpath /home/joelcw/CurrentLx/OldNorse/CS_2.003.04.jar csearch/CorpusSearch"
 
-pceec="~/CurrentLx/OldNorse/hnps/pceec/psd/*.psd"
+pceec="/home/joelcw/CurrentLx/OldNorse/hnps/pceec/psd/*.psd"
 
-midfre="~/CurrentLx/OldNorse/MiddleFrench/*.psd"
+midfre="/home/joelcw/CurrentLx/OldNorse/MiddleFrench/*.psd"
 
-icepahc="~/icecorpus/finished/*.psd"
+icepahc="/home/joelcw/icecorpus/finished/*.psd"
 
-ycoe="~/CurrentLx/OldNorse/historicalsyntaxcourse/YCOE/*.psd"
+ycoe="/home/joelcw/CurrentLx/OldNorse/historicalsyntaxcourse/YCOE/*.psd"
 
-ppcmbe="~/CurrentLx/OldNorse/ppche4/PPCMBE2-RELEASE-1/corpus/psd/*.psd"
-ppcme2="~/CurrentLx/OldNorse/ppche4/PPCME2-RELEASE-4/corpus/psd/*.psd"
-ppceme="~/CurrentLx/OldNorse/ppche4/PPCEME-RELEASE-3/corpus/psd/*/*.psd"
+ppcmbe="/home/joelcw/CurrentLx/OldNorse/ppche4/PPCMBE2-RELEASE-1/corpus/psd/*.psd"
+ppcme2="/home/joelcw/CurrentLx/OldNorse/ppche4/PPCME2-RELEASE-4/corpus/psd/*.psd"
+ppceme="/home/joelcw/CurrentLx/OldNorse/ppche4/PPCEME-RELEASE-3/corpus/psd/*/*.psd"
 
-tychobrahe="/Users/yoelw/CurrentLx/OldNorse/tychobrahe/*psd.txt"
+tychobrahe="/home/joelcw/CurrentLx/OldNorse/tychobrahe/*psd.txt"
 
 #Run Study on PPCHE
 
 
 rm infoTheoryTest.ymeb.cod
-$CS infoTheoryTest.c $ycoe $ppcme2 $ppceme $ppcmbe -out infoTheoryTest.ymeb.cod
+$CS infoTheoryTest.c $ycoe $ppcme2 $ppceme $ppcmbe -out outputs/infoTheoryTest.ymeb.cod
 
 rm infoTheoryTest.ymeb.cod.ooo
 $CS codes.q outputs/infoTheoryTest.ymeb.cod
 
-"Done with study on ppche"
+"Done with query on ppche"
 
 python fixIdFormat.py < outputs/infoTheoryTest.ymeb.cod.ooo > outputs/infoTheoryTest.ymeb.cod.ooo.tmp
 mv outputs/infoTheoryTest.ymeb.cod.ooo.tmp outputs/infoTheoryTest.ymeb.cod.ooo
 
 echo "Fixed codes"
 
+python mergeMetaPpche.py < outputs/infoTheoryTest.ymeb.cod.ooo > outputs/infoTheoryTest.ymeb.cod.ooo.tmp1
+mv outputs/infoTheoryTest.ymeb.cod.ooo.tmp1 outputs/infoTheoryTest.ymeb.cod.ooo
+
+echo "added metadata, done with study on ppche, starting icepahc"
+
 rm outputs/infoTheoryTest.ice.nodates.cod
 $CS infoTheoryTest.c $icepahc -out outputs/infoTheoryTest.ice.nodates.cod
 
-echo "done with study on IcePaHC"
+echo "done with query on IcePaHC"
 
 $CS dateGenre.ice.c outputs/infoTheoryTest.ice.nodates.cod -out outputs/infoTheoryTest.ice.cod
 
@@ -42,11 +47,11 @@ echo "done with dating IcePaHC data"
 rm outputs/infoTheoryTest.ice.cod.ooo
 $CS codes.q outputs/infoTheoryTest.ice.cod
 
-"Done with study on icepahc"
+echo "Done with study on icepahc"
 
 python fixIdFormat.py < outputs/infoTheoryTest.ice.cod.ooo > outputs/infoTheoryTest.ice.cod.ooo.tmp
 mv outputs/infoTheoryTest.ice.cod.ooo.tmp outputs/infoTheoryTest.ice.cod.ooo
 
 echo "Fixed codes"
 
-Rscript myovvo_plotsonly.R
+#Rscript myovvo_plotsonly.R
