5350e544234648914f032a3e7bce808fbaf42cd2
Joel <joel.wallenberg@gmail.com>
2020-05-22 13:42:58 +0100

    new simulation

--- uid_sentSim.R
+++ uid_sentSim.R
@@ -1,8 +1,11 @@
 #note that this script uses the uido and dorm functions from dormUido.R in the same directory
 
 
-#Reads info content strings for each sentence in pyccle into a data frame
-pyccle.bits <- readLines(con=(file="CurrentLx/infoTheory/localCailScripts/foo.txt"))
+#Reads info content strings for each sentence in pyccle into a data frame; I've had to partition pyccle's bits into 5 parts and read each in one at a time or R will crash
+pyccle.bits <- 0
+pyccle.bits <- readLines(con=(file="~/CurrentLx/infoTheory/localCailScripts/x05"))
+
+
 
 
 #Creates a vector of vectors for each sentence in pyccle that is 10 words long
@@ -23,25 +26,45 @@ while (ii <= length(pyccle.bits))
   ii <- ii+1
 }
 
+#i needed to save the vector in two halves so as to clear working memory between runs of the above, so as not to crash my laptop
+pyccle.vecs.df <- as.data.frame(do.call(rbind, pyccle.vecs))
+write.csv(file= "~/CurrentLx/infoTheory/localCailScripts/pyccle.vecs2.df",pyccle.vecs.df)
+
+pyccle.vecs1.df <- read.csv(file="~/CurrentLx/infoTheory/localCailScripts/pyccle.vecs1.df", header=F, row.names = 1)
+
+colnames(pyccle.vecs1.df) <- c("V1","V2","V3","V4","V5","V6","V7","V8","V9","V10")
+
+pyccle.vecs2.df <- read.csv(file="~/CurrentLx/infoTheory/localCailScripts/pyccle.vecs2.df", header=F, row.names = 1)
+
+colnames(pyccle.vecs2.df) <- c("V1","V2","V3","V4","V5","V6","V7","V8","V9","V10")
+
+pyccle.vecsall.df <- rbind(pyccle.vecs1.df,pyccle.vecs2.df)
+
+#convert the dataframe back to a list of vectors for use in the sim below
+pyccle.vecs.list <- as.list(as.data.frame(t(pyccle.vecsall.df)))
+  
+
 
 ii = 1 #initialize counter
 currenttrial = 1 #initialize trial counter
 
 #initialize vectors for each component of each run of the loop
 
-niter <- length(pyccle.vecs) 
+niter <- length(pyccle.vecs.list) 
 
 noises <- vector(length = niter) #proportion of total info in the sentence destroyed by noise
 stringtypes <- vector(length = niter)
 trials <- vector(length = niter)
 absNoise <- vector(length = niter) #absolute amount of info in the sentence destroyed by noise, in bits
+dorms <- vector(length = niter) #dorm for each array, using "dorm" function from dormUido.R
+
 
 
 
 while (currenttrial <= niter)
 {
   
-  probvecinfo <- pyccle.vecs[[currenttrial]]
+  probvecinfo <- pyccle.vecs.list[[currenttrial]]
   
   probvec <- 1/(2^probvecinfo)
   
@@ -53,6 +76,8 @@ while (currenttrial <= niter)
   totalnoise <- sum(noiseinfo)
   totalprobvecinfo <- sum(probvecinfo)
   noiseprop <- totalnoise/totalprobvecinfo
+  probvecDorm <- dorm(probvecinfo)
+  
 
   
 #Doing the same thing, but now the noise will fall in the same position in a vector that is end-heavy for information, but contains the same total info
@@ -63,6 +88,7 @@ while (currenttrial <= niter)
   noise2info <- log2(1/noise2)
   lopsidedinfo <- log2(1/lopsided)
   totalnoise2 <- sum(noise2info)
+  lopsidedDorm <- dorm(lopsidedinfo)
 
 #This next is really a sanity check: this vector contains the same probabilities as the last, so this should be equal to totalprobvecinfo
   totallopsidedinfo <- sum(lopsidedinfo)
@@ -70,42 +96,56 @@ while (currenttrial <= niter)
 
   
   #doing the same thing, but for a vector optimized by the uido algorithm
-  
   uidoinfo <- uido(probvecinfo)
   
   totalnoise4 <- sum(c(uidoinfo[starting], uidoinfo[starting+1], uidoinfo[starting+2]))
   totaluidoinfo <- sum(uidoinfo)
   noise4prop <- totalnoise4/totaluidoinfo
+  uidoDorm <- dorm(uidoinfo)
+  
+  
+  #doing the same thing, but for a randomized vector
+  randominfo <- sample(probvecinfo)
+  
+  totalnoiseRand <- sum(c(randominfo[starting], randominfo[starting+1], randominfo[starting+2]))
+  totalrandominfo <- sum(randominfo)
+  noiseRandprop <- totalnoiseRand/totalrandominfo
+  randomDorm <- dorm(randominfo)
+  
   
   #update vectors for each trial
   noises[ii] <- noiseprop
   absNoise[ii] <- totalnoise
-  stringtypes[ii] <- "random"
+  stringtypes[ii] <- "original"
   trials[ii] <- currenttrial
+  dorms[ii] <- probvecDorm
   
   noises[ii+1] <- noise2prop
   absNoise[ii+1] <- totalnoise2
   stringtypes[ii+1] <- "asymmetric"
   trials[ii+1] <- currenttrial
+  dorms[ii+1] <- lopsidedDorm
   
   noises[ii+2] <- noise4prop
   absNoise[ii+2] <- totalnoise4
   stringtypes[ii+2] <- "uido-optimized"
   trials[ii+2] <- currenttrial
+  dorms[ii+2] <- uidoDorm
   
+  noises[ii+3] <- noiseRandprop
+  absNoise[ii+3] <- totalnoiseRand
+  stringtypes[ii+3] <- "randomized"
+  trials[ii+3] <- currenttrial
+  dorms[ii+3] <- randomDorm
   
-  ii <- ii+3
+  ii <- ii+4
   currenttrial <- currenttrial+1 
 }
 
 #bind vectors into a data frame and add column names
-propnoise.df <- data.frame(trials,noises,absNoise,stringtypes)
+propnoise.df <- data.frame(trials,noises,absNoise,stringtypes,dorms)
 #colnames(propnoise.df) <- c("Trial","NoiseProp","VecType")
 
-
-#Graphing
-
-library(ggplot2)
 
 propnoise.df$trials <- as.numeric(as.character(propnoise.df$trials))
 propnoise.df$noises <- as.numeric(as.character(propnoise.df$noises))
@@ -114,23 +154,16 @@ propnoise.df$noises <- as.numeric(as.character(propnoise.df$noises))
 
 propnoise.df$bigLoss <- ifelse(propnoise.df$noises >= 0.5, 1, 0)
 
-write.csv(propnoise.df,file = "~/constantentropy/uid_sim_run1mil.csv", row.names = F, col.names = T)
+write.csv(propnoise.df,file = "~/constantentropy/uid_sentSimWithRand.csv", row.names = F)
+propnoise.df <- read.csv(file = "~/constantentropy/uid_sentSimWithRand.csv", header=T)
 
-#plotting bigLoss as a binary variable along with other stuff; toggle geom_line() to see the extremes of each stringtype, toggle geom_point() to see all the data, and stat_smooth() to look at the proportion of bits lost
+propnoise.df$stringtypes <- ordered(propnoise.df$stringtypes, levels = c("asymmetric","randomized","original","uido-optimized"))
 
-p <- ggplot(propnoise.df, aes(trials, noises, color=stringtypes)) + 
-  labs(y = "Proportion of Total Bits Lost", x = "\nTrial") + #y = "Proportion of Sentences Where the Majority of Info is Lost" y = "Proportion of Bits Lost"
-  #geom_line() +
-  geom_point(alpha = 1/20) +
-  stat_smooth(size = 1.5) +
-  scale_color_brewer(palette = "Set1") + 
-  ylim(0,1) +
-#  geom_jitter(aes(trials, bigLoss, color=stringtypes), height = 0.15, shape=4) +
-  #stat_smooth(aes(trials, bigLoss, color=stringtypes), linetype="dotdash") +
-  theme_bw() + theme(panel.border = element_blank())
 
-ggsave(p, file = "uid-sim-totalbits.png", width = 8.09, height = 5)
 
+#Graphing
+
+library(ggplot2)
 
 #box plot
 
@@ -138,39 +171,49 @@ p <- ggplot(propnoise.df, aes(stringtypes, noises, group=stringtypes)) +
   scale_y_continuous(name = "Proportion of Total Bits Lost For Each \"Sentence\"") + 
   scale_x_discrete(name = "\nOrder") + 
   geom_point(alpha = 1/25) + 
-  geom_boxplot(fill=c("purple","green","yellow")) +
+  geom_boxplot(fill=c("purple","green","red","yellow")) +
   #geom_jitter(width = 0.3) +
   theme_bw() + 
   theme(panel.border = element_blank())
 
-ggsave(p, file = "~/constantentropy/uid-sim-totalbits1mil.png", width = 8.09, height = 5)
+ggsave(p, file = "~/constantentropy/uid-sentSim-totalbits.png", width = 8.09, height = 5)
 
 
 #plotting bigLoss in a bar chart
 
-####Old way:
-q <- ggplot(propnoise.df[propnoise.df$bigLoss==1,], aes(stringtypes)) + 
-  scale_y_continuous(name = "Number of Sentences with > 50% Information Lost", limits = c(0,1000) ) + 
-  scale_x_discrete(name = "\nOrder") + 
-  #geom_point(alpha = 1/25) + 
-  geom_bar(fill=c("purple","green"), color="black") +
-  #geom_jitter(width = 0.3) +
-  theme_bw() + 
-  theme(panel.border = element_blank())
 
-#####New way
-propnoise.bar.df <- data.frame(stringtypes=c("asymmetric","random","uido-optimized"),NoSentences=c(sum(propnoise.df[stringtypes=="asymmetric",]$bigLoss),sum(propnoise.df[stringtypes=="random",]$bigLoss),sum(propnoise.df[stringtypes=="uido-optimized",]$bigLoss)))
+
+#####bar plot
+propnoise.bar.df <- data.frame(stringtypes=c("asymmetric","randomized","original","uido-optimized"),NoSentences=c(sum(propnoise.df[stringtypes=="asymmetric",]$bigLoss),sum(propnoise.df[stringtypes=="randomized",]$bigLoss),sum(propnoise.df[stringtypes=="original",]$bigLoss),sum(propnoise.df[stringtypes=="uido-optimized",]$bigLoss)))
+
+propnoise.bar.df$stringtypes <- ordered(propnoise.bar.df$stringtypes, levels = c("asymmetric","randomized","original","uido-optimized"))
 
 q <- ggplot(propnoise.bar.df, aes(stringtypes,NoSentences)) + 
   scale_y_continuous(name = "Number of Sentences with > 50% Information Lost", limits = c(0,10000) ) + 
   scale_x_discrete(name = "\nOrder") + 
   #geom_point(alpha = 1/25) + 
-  geom_bar(fill=c("purple","green","yellow"), color="black", stat="identity") +
+  geom_bar(fill=c("purple","green","red","yellow"), color="black", stat="identity") +
   #geom_jitter(width = 0.3) +
   theme_bw() + 
   theme(panel.border = element_blank())
 
-ggsave(q, file = "~/constantentropy/uid-sim-majority1mil.png", width = 8.09, height = 5)
+ggsave(q, file = "~/constantentropy/uid-sentSim-majority.png", width = 8.09, height = 5)
+
+
+#box plot of dorms by stringtype
+
+k <- ggplot(propnoise.df, aes(stringtypes, dorms, group=stringtypes)) + 
+  scale_y_continuous(name = "DORM for each sentence-version") + 
+  scale_x_discrete(name = "\nOrder") + 
+  geom_point(alpha = 1/25) + 
+  geom_boxplot(fill=c("purple","green","red","yellow")) +
+  #geom_jitter(width = 0.3) +
+  theme_bw() + 
+  theme(panel.border = element_blank())
+
+ggsave(p, file = "~/constantentropy/uid-sentSim-totalbits.png", width = 8.09, height = 5)
+
+
 
 #modeling
 
@@ -218,82 +261,5 @@ var(propnoise.df[propnoise.df$stringtypes == "random",]$zAbsNoise)
 var(propnoise.df[propnoise.df$stringtypes == "asymmetric",]$zAbsNoise)
 
 
-#Test for Normality of small samples from Zipf distribution
-library(zoo)
-
-Brown.bits <- log2(1/Brown.probs)
-
-
-niter <- 1000 
-ii <- 1
-pvalue.raw <- vector(length = niter) #proportion of total info in the sentence destroyed by noise
-pvalue.rollmean <- vector(length = niter)
-
-
-while (ii <= niter)
-{
-
-  foo <- sample(Brown.bits, 10, replace = T, prob = Brown.probs)
-  rolling <- rollmean(foo,2)
-  
-  #shapiro-wilk test for normality
-  pvalue.raw[ii] <- shapiro.test(foo)$p.value
-  pvalue.rollmean[ii] <- shapiro.test(rolling)$p.value
-  
-  ii <- ii+1
-  
-}
-
-#bind vectors into a data frame and add column names
-tests.df <- data.frame(pvalue.raw,pvalue.rollmean)
-
-
-#Is p.value > various alphas for raw array and for its rolling mean with window 2
-
-tests.df$alpha5raw <- ifelse(tests.df$pvalue.raw >= 0.5, 1, 0)
-tests.df$alpha5rolling <- ifelse(tests.df$pvalue.rollmean >= 0.5, 1, 0)
-
-tests.df$alpha05raw <- ifelse(tests.df$pvalue.raw >= 0.05, 1, 0)
-tests.df$alpha05rolling <- ifelse(tests.df$pvalue.rollmean >= 0.05, 1, 0)
-
-tests.df$alpha2raw <- ifelse(tests.df$pvalue.raw >= 0.2, 1, 0)
-tests.df$alpha2rolling <- ifelse(tests.df$pvalue.rollmean >= 0.2, 1, 0)
-
-
-#Same thing, but for samples of 10 sequential words
-niter <- 1000 
-ii <- 1
-pvalue.raw <- vector(length = niter) #proportion of total info in the sentence destroyed by noise
-pvalue.rollmean <- vector(length = niter)
-
-
-while (ii <= niter)
-{
-  
-  starting <- sample(1:(length(Brown.bits)-9), size=1)
-  
-  foo <- Brown.bits[starting:(starting+9)]
-  rolling <- rollmean(foo,2)
-  
-  #shapiro-wilk test for normality
-  pvalue.raw[ii] <- shapiro.test(foo)$p.value
-  pvalue.rollmean[ii] <- shapiro.test(rolling)$p.value
-  
-  ii <- ii+1
-  
-}
-
-#bind vectors into a data frame and add column names
-testsSeq.df <- data.frame(pvalue.raw,pvalue.rollmean)
-
-
-#Is p.value > various alphas for raw array and for its rolling mean with window 2
-
-testsSeq.df$alpha5raw <- ifelse(testsSeq.df$pvalue.raw >= 0.5, 1, 0)
-testsSeq.df$alpha5rolling <- ifelse(testsSeq.df$pvalue.rollmean >= 0.5, 1, 0)
-
-testsSeq.df$alpha05raw <- ifelse(testsSeq.df$pvalue.raw >= 0.05, 1, 0)
-testsSeq.df$alpha05rolling <- ifelse(testsSeq.df$pvalue.rollmean >= 0.05, 1, 0)
-
-testsSeq.df$alpha2raw <- ifelse(testsSeq.df$pvalue.raw >= 0.2, 1, 0)
-testsSeq.df$alpha2rolling <- ifelse(testsSeq.df$pvalue.rollmean >= 0.2, 1, 0)
+#Calculate DORM for each stringtype
+propnoise.df$dorm <- propnoise.df$
