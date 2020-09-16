181f1fb93219fc451e2659bcc480ccabddc95759
Joel <joel.wallenberg@gmail.com>
2020-09-16 14:20:52 +0100

    added highlow run

--- rocExp.R
+++ rocExp.R
@@ -1,28 +1,75 @@
 library(ggplot2)
 library(ggridges)
 
-salsa <- read.csv(file="~/CurrentLx/newcastleModules/MRes/salsabila/salsaData.csv", header=T)
+salsa <- read.csv(file="~/Documents/salsaData.csv", header=T)
 
-salsa <- subset(salsa, Age != "" && Age != "unknown")
+partInfo <- data.frame(salsa$ParticipantIdentifier,salsa$Age,salsa$Gender,salsa$Low.Start.High.Start,salsa$Clump.Even,salsa$NativeSpeaker,salsa$EngAoA,salsa$Bilingual,salsa$WorkerType,salsa$TimeToCompletion)
 
-salsa <- subset(salsa, Clump.Even != "")
+colnames(partInfo) <- c("Participant","Age","Sex","LowOrHighStart","ClumpedOrEven","NativeSpeaker","EngAoA","Bilingual","WorkerType","TimeToCompletion")
 
-salsa <- droplevels(salsa)
+highlowrun <- read.csv(file="~/Documents/allBrokenDownByFrequency.csv", header=T)
 
-salsa$Age <- as.numeric(as.character(salsa$Age))
+highlowrun$File <- as.character(highlowrun$File)
 
-salsa$TimeToCompletion <- as.numeric(as.character(salsa$TimeToCompletion))
+#make another column for whether the run is on high or low freq words
+pattern <- "^(.*)_responses_((low)|(high)).xlsx"
+highlowrun$Frequency <- gsub(pattern,"\\2",highlowrun$File)
 
+#make another column for participant id
+highlowrun$Participant <- gsub(pattern,"\\1",highlowrun$File)
+
+#merge the participant info from salsa with the highlowrun data
+highlowFull <- merge(highlowrun, partInfo, by=c("Participant"))
+
+#salsa <- subset(salsa, Age != "" && Age != "unknown")
+#salsa <- subset(salsa, Clump.Even != "")
+#salsa <- droplevels(salsa)
+highlowFull <- subset(highlowFull, Age != "unknown")
+highlowFull <- subset(highlowFull, Age != "")
+highlowFull <- subset(highlowFull, ClumpedOrEven != "")
+highlowFull <- droplevels(highlowFull)
+
+
+#salsa$Age <- as.numeric(as.character(salsa$Age))
+#salsa$TimeToCompletion <- as.numeric(as.character(salsa$TimeToCompletion))
+highlowFull$Age <- as.numeric(as.character(highlowFull$Age))
+highlowFull$TimeToCompletion <- as.numeric(as.character(highlowFull$TimeToCompletion))
+highlowFull$Frequency <- as.factor(highlowFull$Frequency)
+
+write.csv(highlowFull, file="/Documents/highlowFull.csv", row.names = F)
 
 #box plots
-ggplot(salsa, aes(Clump.Even, R, group=Clump.Even)) + 
+ggplot(highlowFull, aes(ClumpedOrEven, R, group=ClumpedOrEven)) + 
   scale_y_continuous(name = "R") + 
   scale_x_discrete(name = "\nExperimental Condition") + 
   geom_point(alpha = 1/25) + 
-  geom_boxplot(fill=c("purple","green")) +
+  geom_boxplot(fill=c("purple","green","purple","green")) +
   #geom_jitter(width = 0.3) +
+  facet_wrap(~Frequency) +
   theme_bw() + 
   theme(panel.border = element_blank())
+
+ggplot(highlowFull, aes(ClumpedOrEven, dprime, group=ClumpedOrEven)) + 
+  scale_y_continuous(name = "d\'") + 
+  scale_x_discrete(name = "\nExperimental Condition") + 
+  geom_point(alpha = 1/25) + 
+  geom_boxplot(fill=c("purple","green","purple","green")) +
+  #geom_jitter(width = 0.3) +
+  facet_wrap(~Frequency) +
+  theme_bw() + 
+  theme(panel.border = element_blank())
+
+ggplot(highlowFull, aes(ClumpedOrEven, AUROC, group=ClumpedOrEven)) + 
+  scale_y_continuous(name = "d\'") + 
+  scale_x_discrete(name = "\nExperimental Condition") + 
+  geom_point(alpha = 1/25) + 
+  geom_boxplot(fill=c("purple","green","purple","green")) +
+  #geom_jitter(width = 0.3) +
+  facet_wrap(~Frequency) +
+  theme_bw() + 
+  theme(panel.border = element_blank())
+
+
 
 ggplot(salsa, aes(Clump.Even, TimeToCompletion, group=Clump.Even)) + 
   scale_y_continuous(name = "Time To Completion (mins)") + 
@@ -88,3 +135,31 @@ salsa.timefit.Gender.Age.Start <- glm(TimeToCompletion~Gender+Age+Bilingual+Low.
 anova(salsa.timefit.Gender.Age.Start,salsa.timefit.Gender.Age.Start.Clump, test = "Chisq")
 AIC(salsa.timefit.Gender.Age.Start)
 AIC(salsa.timefit.Gender.Age.Start.Clump)
+
+####Models for the high/low separated data
+library(lme4)
+library(lmerTest)
+highlowFull.Rfit.Sex.Age.Start.Clump.Freq.FreqClump <- lmer(R~(1|Participant)+Sex+Age+Bilingual+LowOrHighStart+ClumpedOrEven+Frequency+Frequency:ClumpedOrEven, data=highlowFull)
+highlowFull.Rfit.Sex.Age.Start.Clump.Freq <- lmer(R~(1|Participant)+Sex+Age+Bilingual+LowOrHighStart+ClumpedOrEven+Frequency, data=highlowFull)
+summary(highlowFull.Rfit.Sex.Age.Start.Clump.Freq.FreqClump)
+anova(highlowFull.Rfit.Sex.Age.Start.Clump.Freq.FreqClump,highlowFull.Rfit.Sex.Age.Start.Clump.Freq, test="Chisq")
+AIC(highlowFull.Rfit.Sex.Age.Start.Clump.Freq.FreqClump) - AIC(highlowFull.Rfit.Sex.Age.Start.Clump.Freq)
+
+highlowFull.Rfit.Sex.Age.Start.Clump <- lmer(R~(1|Participant)+Sex+Age+Bilingual+LowOrHighStart+ClumpedOrEven, data=highlowFull)
+highlowFull.Rfit.Sex.Age.Start.Freq <- lmer(R~(1|Participant)+Sex+Age+Bilingual+LowOrHighStart+Frequency, data=highlowFull)
+anova(highlowFull.Rfit.Sex.Age.Start.Freq,highlowFull.Rfit.Sex.Age.Start.Clump.Freq, test="Chisq")
+anova(highlowFull.Rfit.Sex.Age.Start.Clump,highlowFull.Rfit.Sex.Age.Start.Clump.Freq, test="Chisq")
+
+#same for dprime, ie familiarity
+highlowFull.dfit.Sex.Age.Start.Clump.Freq.FreqClump <- lmer(dprime~(1|Participant)+Sex+Age+Bilingual+LowOrHighStart+ClumpedOrEven+Frequency+Frequency:ClumpedOrEven, data=highlowFull)
+highlowFull.dfit.Sex.Age.Start.Clump.Freq <- lmer(dprime~(1|Participant)+Sex+Age+Bilingual+LowOrHighStart+ClumpedOrEven+Frequency, data=highlowFull)
+summary(highlowFull.dfit.Sex.Age.Start.Clump.Freq.FreqClump)
+anova(highlowFull.dfit.Sex.Age.Start.Clump.Freq.FreqClump,highlowFull.dfit.Sex.Age.Start.Clump.Freq, test="Chisq")
+AIC(highlowFull.dfit.Sex.Age.Start.Clump.Freq.FreqClump) - AIC(highlowFull.dfit.Sex.Age.Start.Clump.Freq)
+
+#same for AUROC
+highlowFull.auroc.Sex.Age.Start.Clump.Freq.FreqClump <- lmer(AUROC~(1|Participant)+Sex+Age+Bilingual+LowOrHighStart+ClumpedOrEven+Frequency+Frequency:ClumpedOrEven, data=highlowFull)
+highlowFull.auroc.Sex.Age.Start.Clump.Freq <- lmer(AUROC~(1|Participant)+Sex+Age+Bilingual+LowOrHighStart+ClumpedOrEven+Frequency, data=highlowFull)
+summary(highlowFull.auroc.Sex.Age.Start.Clump.Freq.FreqClump)
+anova(highlowFull.auroc.Sex.Age.Start.Clump.Freq.FreqClump,highlowFull.auroc.Sex.Age.Start.Clump.Freq, test="Chisq")
+AIC(highlowFull.auroc.Sex.Age.Start.Clump.Freq.FreqClump) - AIC(highlowFull.auroc.Sex.Age.Start.Clump.Freq)
