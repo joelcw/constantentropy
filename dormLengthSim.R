e45596ea119cff94c48f102effe1266735f44921
Joel <joel.wallenberg@gmail.com>
2020-12-11 16:36:37 +0000

    created simulation for vectors of different lengths

---
+++ dormLengthSim.R
@@ -0,0 +1,269 @@
+#This script creates vectors of various lengths consisting of random, independent information content values,
+#and calculates their dorm() and uido(dorm()) - dorm() for comparison.
+
+library(devtools)
+source_url('https://raw.githubusercontent.com/rbailes/DORM-UIDO/f778fd66c30ce8a127dfac526084db87abeb8cdc/dormUido.R')
+
+
+
+
+minStringLength = 10 #first length to be considered
+maxStringLength = 100 #longest string to be considered
+ii <- 1 #initialize counter
+niter <- 1000 #number iterations
+current = 1 #initialize iteration counter
+
+
+
+foo <- matrix(nc=5,nr=((maxStringLength-minStringLength+1)*niter),0)
+
+strings.df <- data.frame(foo)
+
+colnames(strings.df) <- c("Iteration","Lengths","Dorm","Uido","DormUido")
+
+
+
+while (current <= niter)
+{
+  
+  stringLength <- minStringLength
+  
+  while (stringLength <= maxStringLength)
+    {
+      
+      string <- -log2(runif(n=stringLength,min=0,max=1))
+      stringDorm <- dorm(string)
+      stringUido <- dorm(uido(string))
+      stringDormUido <- stringDorm-stringUido
+      
+      strings.df$Iteration[ii] <- current
+      strings.df$Lengths[ii] <- stringLength
+      strings.df$Dorm[ii] <- stringDorm
+      strings.df$Uido[ii] <- stringUido
+      strings.df$DormUido[ii] <- stringDormUido
+
+      
+      stringLength <- stringLength+1
+      ii <- ii+1
+    }
+  
+  current <- current+1    
+  
+}
+
+#Graphing
+
+library(ggplot2)
+
+
+
+#box plot
+
+p <- ggplot(propnoise.df, aes(stringtypes, noises, group=stringtypes)) + 
+  scale_y_continuous(name = "Proportion of Total Bits Lost For Each \"Sentence\"") + 
+  scale_x_discrete(name = "\nOrder") + 
+  geom_point(alpha = 1/25) + 
+  geom_boxplot(fill=c("purple","green","red","yellow")) +
+  #geom_jitter(width = 0.3) +
+  theme_bw() + 
+  theme(panel.border = element_blank())
+
+ggsave(p, file = "~/constantentropy/uid-hansardSim-totalbits.png", width = 8.09, height = 5)
+
+
+#plotting bigLoss in a bar chart
+
+
+
+#####bar plot
+propnoise.bar.df <- data.frame(stringtypes=c("asymmetric","randomized","original","uido-optimized"),NoSentences=c(sum(propnoise.df[stringtypes=="asymmetric",]$bigLoss),sum(propnoise.df[stringtypes=="randomized",]$bigLoss),sum(propnoise.df[stringtypes=="original",]$bigLoss),sum(propnoise.df[stringtypes=="uido-optimized",]$bigLoss)))
+
+propnoise.bar.df$stringtypes <- ordered(propnoise.bar.df$stringtypes, levels = c("asymmetric","randomized","original","uido-optimized"))
+
+q <- ggplot(propnoise.bar.df, aes(stringtypes,NoSentences)) + 
+  scale_y_continuous(name = "Number of Sentences with > 50% Information Lost", limits = c(0,10000) ) + 
+  scale_x_discrete(name = "\nOrder") + 
+  #geom_point(alpha = 1/25) + 
+  geom_bar(fill=c("purple","green","red","yellow"), color="black", stat="identity") +
+  #geom_jitter(width = 0.3) +
+  theme_bw() + 
+  theme(panel.border = element_blank())
+
+ggsave(q, file = "~/constantentropy/uid-hansardSim-majority.png", width = 8.09, height = 5)
+
+
+#box plot of dorms by stringtype
+
+k <- ggplot(propnoise.df, aes(stringtypes, dorms, group=stringtypes)) + 
+  scale_y_continuous(name = "DORM for each sentence-version") + 
+  scale_x_discrete(name = "\nOrder") + 
+  geom_point(alpha = 1/25) + 
+  geom_boxplot(fill=c("purple","green","red","yellow")) +
+  #geom_jitter(width = 0.3) +
+  theme_bw() + 
+  theme(panel.border = element_blank())
+
+ggsave(k, file = "~/constantentropy/uid-hansardSim-dorms.png", width = 8.09, height = 5)
+
+
+
+#modeling
+#get rid of the ordering of the levels of stringtypes, as that was just for plotting purposes
+propnoise.df$stringtypes <- as.factor(as.character(propnoise.df$stringtypes))
+
+library(lme4)
+#simple linear
+totalbits.fit <- lm(absNoise~stringtypes, data=propnoise.df)
+summary(totalbits.fit)
+
+#linear with DORM as predictor
+totalbits.fit2 <- lm(absNoise~dorms, data=propnoise.df)
+summary(totalbits.fit2)
+
+#linear for DORM by stringtypes
+totalbits.fit3 <- lm(dorms~stringtypes, data=propnoise.df)
+summary(totalbits.fit3)
+
+
+
+#simple logistic
+failure.fit <- glm(bigLoss~stringtypes, family=binomial, data=propnoise.df)
+summary(failure.fit)
+
+#logistic with DORM as predictor instead
+failure.fit2 <- glm(bigLoss~dorms, family=binomial, data=propnoise.df)
+summary(failure.fit2)
+
+
+
+#change contrasts so we can compare uido to randomized
+propnoise.df$stringtypes <- relevel(propnoise.df$stringtypes, ref="original")
+
+failure.relevel.fit <- glm(bigLoss~stringtypes, family=binomial, data=propnoise.df)
+summary(failure.relevel.fit)
+
+
+#Estimate variances on the Zed values for info lost to noise for each string type:
+var(propnoise.df[propnoise.df$stringtypes == "uido-optimized",]$zAbsNoise)
+var(propnoise.df[propnoise.df$stringtypes == "random",]$zAbsNoise)
+var(propnoise.df[propnoise.df$stringtypes == "asymmetric",]$zAbsNoise)
+
+#######################
+####Random noise simulation; same as above, but the three noise events do not have to be sequential (though they may be)
+
+
+ii = 1 #initialize counter
+currenttrial = 1 #initialize trial counter
+
+#initialize vectors for each component of each run of the loop
+
+niter <- length(hansard.vecs.list) 
+
+noises <- vector(length = niter) #proportion of total info in the sentence destroyed by noise
+stringtypes <- vector(length = niter)
+trials <- vector(length = niter)
+absNoise <- vector(length = niter) #absolute amount of info in the sentence destroyed by noise, in bits
+dorms <- vector(length = niter) #dorm for each array, using "dorm" function from dormUido.R
+
+
+
+
+while (currenttrial <= niter)
+{
+  
+  probvecinfo <- hansard.vecs.list[[currenttrial]]
+  
+  probvec <- 1/(2^probvecinfo)
+  
+  #Now unlike the simulation above, this just selects three string positions from anywhere in the string, without replacement
+  noisePositions <- sample(1:length(probvec), size=3, replace=FALSE) #remember, lists in R perversely start at offset 1, not 0
+  
+  noise <- c(probvec[noisePositions[1]], probvec[noisePositions[2]], probvec[noisePositions[3]])
+  noiseinfo <- log2(1/noise)
+  probvecinfo <- log2(1/probvec)
+  totalnoise <- sum(noiseinfo)
+  totalprobvecinfo <- sum(probvecinfo)
+  noiseprop <- totalnoise/totalprobvecinfo
+  probvecDorm <- dorm(probvecinfo)
+  
+  
+  
+  #Doing the same thing, but now the noise will fall in the same position in a vector that is end-heavy for information, but contains the same total info
+  lopsided <- sort(probvec)
+  
+  #It is important that the starting point in the vector is the same for both cases, so that each comparison of noise is a true comparison
+  noise2 <- c(lopsided[noisePositions[1]], lopsided[noisePositions[2]], lopsided[noisePositions[3]])
+  noise2info <- log2(1/noise2)
+  lopsidedinfo <- log2(1/lopsided)
+  totalnoise2 <- sum(noise2info)
+  lopsidedDorm <- dorm(lopsidedinfo)
+  
+  #This next is really a sanity check: this vector contains the same probabilities as the last, so this should be equal to totalprobvecinfo
+  totallopsidedinfo <- sum(lopsidedinfo)
+  noise2prop <- totalnoise2/totallopsidedinfo
+  
+  
+  #doing the same thing, but for a vector optimized by the uido algorithm
+  uidoinfo <- uido(probvecinfo)
+  
+  totalnoise4 <- sum(c(uidoinfo[noisePositions[1]], uidoinfo[noisePositions[2]], uidoinfo[noisePositions[3]]))
+  totaluidoinfo <- sum(uidoinfo)
+  noise4prop <- totalnoise4/totaluidoinfo
+  uidoDorm <- dorm(uidoinfo)
+  
+  
+  #doing the same thing, but for a randomized vector
+  randominfo <- sample(probvecinfo)
+  
+  totalnoiseRand <- sum(c(randominfo[noisePositions[1]], randominfo[noisePositions[2]], randominfo[noisePositions[3]]))
+  totalrandominfo <- sum(randominfo)
+  noiseRandprop <- totalnoiseRand/totalrandominfo
+  randomDorm <- dorm(randominfo)
+  
+  
+  #update vectors for each trial
+  noises[ii] <- noiseprop
+  absNoise[ii] <- totalnoise
+  stringtypes[ii] <- "original"
+  trials[ii] <- currenttrial
+  dorms[ii] <- probvecDorm
+  
+  noises[ii+1] <- noise2prop
+  absNoise[ii+1] <- totalnoise2
+  stringtypes[ii+1] <- "asymmetric"
+  trials[ii+1] <- currenttrial
+  dorms[ii+1] <- lopsidedDorm
+  
+  noises[ii+2] <- noise4prop
+  absNoise[ii+2] <- totalnoise4
+  stringtypes[ii+2] <- "uido-optimized"
+  trials[ii+2] <- currenttrial
+  dorms[ii+2] <- uidoDorm
+  
+  noises[ii+3] <- noiseRandprop
+  absNoise[ii+3] <- totalnoiseRand
+  stringtypes[ii+3] <- "randomized"
+  trials[ii+3] <- currenttrial
+  dorms[ii+3] <- randomDorm
+  
+  ii <- ii+4
+  currenttrial <- currenttrial+1 
+}
+
+#bind vectors into a data frame and add column names
+propnoise.df <- data.frame(trials,noises,absNoise,stringtypes,dorms)
+#colnames(propnoise.df) <- c("Trial","NoiseProp","VecType")
+
+
+propnoise.df$trials <- as.numeric(as.character(propnoise.df$trials))
+propnoise.df$noises <- as.numeric(as.character(propnoise.df$noises))
+propnoise.df$dorms <- as.numeric(as.character(propnoise.df$dorms))
+
+#add a column into the dataframe recoding for losing more than 50% of the info
+
+propnoise.df$bigLoss <- ifelse(propnoise.df$noises >= 0.5, 1, 0)
+
+write.csv(propnoise.df,file = "~/constantentropy/uid_hansardSimOneWordNoises.csv", row.names = F)
+propnoise.df <- read.csv(file = "~/constantentropy/uid_hansardSimOneWordNoises.csv", header=T)
+
+propnoise.df$stringtypes <- ordered(propnoise.df$stringtypes, levels = c("asymmetric","randomized","original","uido-optimized"))
