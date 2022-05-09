0853b0825e50e31b3e06dc4f61bd477e3dec9f3e
Joel <joel.wallenberg@gmail.com>
2022-05-09 09:40:47 +0100

    changed shell script so that Anton can use ID codes

---
+++ infoTheoryTestV2.sh
@@ -0,0 +1,26 @@
+CS="java -classpath /home/joelcw/CurrentLx/OldNorse/CS_2.003.04.jar csearch/CorpusSearch"
+
+icepahc="/home/joelcw/icecorpus/finished/*.psd"
+
+rm outputs/infoTheoryTestV2.ice.nodates.cod
+$CS infoTheoryTestV2.c $icepahc -out outputs/infoTheoryTestV2.ice.nodates.cod
+
+echo "done with query on IcePaHC"
+
+$CS dateGenreV2.ice.c outputs/infoTheoryTestV2.ice.nodates.cod -out outputs/infoTheoryTestV2.ice.cod
+
+echo "done with dating IcePaHC data"
+
+#Some things are commented out below so that we can make a raw version with full IDs that Anton can use
+
+#rm outputs/infoTheoryTestV2.ice.cod.ooo
+$CS codes.q outputs/infoTheoryTestV2.ice.raw.cod
+
+echo "Done with study on icepahc"
+
+#python fixIdFormat.py < outputs/infoTheoryTestV2.ice.cod.ooo > outputs/infoTheoryTestV2.ice.cod.ooo.tmp
+#mv outputs/infoTheoryTestV2.ice.cod.ooo.tmp outputs/infoTheoryTestV2.ice.cod.ooo
+
+echo "Fixed codes"
+
+#Rscript myovvo_plotsonly.R
