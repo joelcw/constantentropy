18350735620576c708a6cc7cdf650f3f4f793ed0
Joel <joel.wallenberg@gmail.com>
2022-06-02 09:32:13 +0100

    added slides

--- fixIdFormat.py
+++ fixIdFormat.py
@@ -1,16 +1,17 @@
 #18July2018
+#Modified 30May2022 to include tree IDs
 #Script takes the output of CS extracting codes with printing token ids, and converts the id into a coding column for text; so it changes @ to : and removes token number, and makes all ids lower case for simplicity in dealing with metadata.
 
 import sys,string,re
 
 #Note that the second group below is made non-greedy because of some YCOE ids that have another comma in them later on...but check this behaviour 
 
-idregex = re.compile("(@)(.*?)(,.*)$")
+idregex = re.compile("(@)([0-9x]+\.)([A-Za-z]+?)(\..*?)(,\.)([0-9]+)$")
 
 #function returning id in the right format 
 
 def textid(matchobj):
-    return (":%s" % matchobj.group(2))
+    return (":%s:%s" % (matchobj.group(3),matchobj.group(6)))
 
 lines = sys.stdin.readlines()
 
